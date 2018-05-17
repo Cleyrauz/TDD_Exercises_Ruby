19da94f102a00582c2f4de95a202ccbc19d8dc94
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-05-17 22:15:12 +0100

    Tested For a given person, remove a specific name from their list of friends

--- day_4/friends_spec.rb
+++ day_4/friends_spec.rb
@@ -95,7 +95,10 @@ end
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
-
+def test_remove_friend_list
+  remove_friend(@person2, "Fred")
+  assert_equal(0, @person2[:friends].length)
+end
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
